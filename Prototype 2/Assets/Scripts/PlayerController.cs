9ccb3b770056e25dfd4d7049f062feab44dbb1d9
William <long.le@indigames.net>
2022-04-19 16:27:41 +0700

    Lesson 2.1 - Player Positioning

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float hInput;
+    public float speed = 10;
+    public float xRange = 25;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+    }
+}
