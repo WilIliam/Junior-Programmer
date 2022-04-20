625164343b3172e7ebb8ab8697b76eb9b77a27e2
WilIliam <long.le@indigames.net>
2022-04-20 15:25:44 +0700

    Lesson 3.4 - Particles and Sound Effects

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
 {
 	private Rigidbody playerRb;
 	private Animator animator;
+	private AudioSource playerAudio;
+
+	public ParticleSystem exploreP;
+	public ParticleSystem dirtP;
+	public AudioClip jumpSound;
+	public AudioClip crashSound;
 
 	public float jumpForce = 2000;
 	public float gravityModifier = 9.807f;
@@ -17,6 +23,7 @@ public class PlayerController : MonoBehaviour
 	{
 		playerRb = GetComponent<Rigidbody>();
 		animator = GetComponent<Animator>();
+		playerAudio = GetComponent<AudioSource>();
 		Physics.gravity *= gravityModifier;
 	}
 
@@ -28,6 +35,8 @@ public class PlayerController : MonoBehaviour
 			playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 			isOnGround = false;
 			animator.SetTrigger("Jump_trig");
+			dirtP.Stop();
+			playerAudio.PlayOneShot(jumpSound,1);
 		}
 	}
 
@@ -36,12 +45,16 @@ public class PlayerController : MonoBehaviour
 		if (collision.gameObject.CompareTag("Ground"))
 		{
 			isOnGround = true;
+			dirtP.Play();
 		}else if (collision.gameObject.CompareTag("Obstacle"))
 		{
 			Debug.Log("Game over");
 			gameOver = true;
 			animator.SetBool("Death_b",true);
 			animator.SetInteger("DeathType_int", 1);
+			exploreP.Play();
+			dirtP.Stop();
+			playerAudio.PlayOneShot(crashSound,1);
 		}
 	}
 }
