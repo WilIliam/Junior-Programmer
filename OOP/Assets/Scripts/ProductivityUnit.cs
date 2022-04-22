ec177cf33d306a1e4d3f7a958e9d744189ffa441
WilIliam <long.le@indigames.net>
2022-04-22 11:29:08 +0700

    Inheritance and polymorphism in object-oriented programming

---
+++ OOP/Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,62 @@
+/*
+* Create by William (c)
+* https://github.com/Long18
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+
+    #region Variables
+
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+    #endregion
+
+    #region Unity Mehods
+
+
+    #endregion
+
+    #region Class
+
+    protected override void BuildingInRange()
+    {
+        // start of new code
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+        // end of new code
+    }
+
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
+    }
+
+    public override void GoTo(Building target)
+    {
+        ResetProductivity(); // call your new method
+        base.GoTo(target); // run method from base class
+    }
+
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
+    }
+    #endregion
+}
